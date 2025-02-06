33ea6fb0e9aa7cc045d553dec5613df40318b938
MM <M_Mondal@outlook.com>
2025-02-05 20:59:25 -0500

    module 3 ct part2 - updated

--- Module3/ct_part2.py
+++ Module3/ct_part2.py
@@ -7,9 +7,7 @@ Ask the user for the time now (in hours) and then ask for the number of hours to
 Your program should output what the time will be on a 24-hour clock when the alarm goes off.
 '''
 
-
 from os import system, name
-from datetime import datetime, timedelta
 
 
 def clearScreen():    
@@ -17,38 +15,32 @@ def clearScreen():
         _ = system('cls')    
     else:             # for non-windows system
         _ = system('clear')
+    return
 
-# Get current system date, time
-def getCurrentLocalDatetime():        
-    now = datetime.now()
-    hour = now.strftime("%H") # Get the hour and date
-    date = now.strftime("%d day of %B in the year %Y")    
-    print("It is hour", hour, "of the", date) # Show current date/time
-    return now
 
-# Calculate the date, time after the waiting hours
-def calculateNewDatetime(currentTime, hoursToWait):    
-    newTime = currentTime + timedelta(hours=hoursToWait) # Calculate the new time    
-    hour = newTime.strftime("%H") # Get the hour and date
-    date = newTime.strftime("%d day of %B in the year %Y")    
-    print("After", hoursToWait, "hours, it will be hour", hour, "of the", date)   # show the result
-
+def getUserInput2DigitsHourTime(userMsg):        
+    userInput2DigitHours = 0
+    validInput = False
+    while not validInput:
+        try:
+            userInput2DigitHours = int(input(userMsg))
+            if userInput2DigitHours < 0 or userInput2DigitHours > 99:                
+                print("(Invalid Input), try again! >>> ")
+            else:
+                validInput = True
+        except ValueError:
+            print("(Invalid Input), try again! >>> ")
+    return userInput2DigitHours
+    
 
 # the main function that calls other functions and does the final work
 def ct_part2():
-    clearScreen()
-    curTime = getCurrentLocalDatetime()
-    print("How many whole hours would you like to wait? >>> ", end='') # take user input
-    validInput = False
-    while not validInput:        
-        try:
-            hoursToWait = int(input())
-            validInput = True
-        except ValueError:
-            print("(Invalid Input), try again! How many whole hours would you like to wait? >>> ", end='')
-            pass
-    
-    calculateNewDatetime(curTime, hoursToWait) # Calculate new datetime
+    clearScreen()    
+    currrentTime2DigitUserInput = getUserInput2DigitsHourTime("Please enter 2 digit hours for the current time: ")
+    waitTime2DigitUserInput = getUserInput2DigitsHourTime("Please enter 2 digit hours for how long you want to wait: ")
+    time2DigitAlarm = (currrentTime2DigitUserInput + waitTime2DigitUserInput)%24
+    print('2 digit hour time when alarm will go off is: ', str(time2DigitAlarm))
+    return
 
 if __name__ == "__main__":
     ct_part2()
