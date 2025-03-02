7164cf61fb744e00848fac68cf0511458ee9aced
MM <M_Mondal@outlook.com>
2025-03-02 18:17:21 -0500

    module 8 pp - added

--- Module8/module8_shoppingCart.py
+++ Module8/module8_shoppingCart.py
@@ -24,10 +24,12 @@ class StoreItem:
         self.Price: float = price                       # Item Price
 
 # This represents the container of all items in the store
-# This dictionary has Key as 'Item Name' and Value as 'Store item' object as defined above.
+# This dictionary has Key as 'Item Name' and Value as 'StoreItem' object as defined above.
+# This dictionary has items initialized with default values.
 # This should not be part of the Cart.
 # This should be a component of the Store.
 # This is not required, per requirement.
+# I'll use these default values in 'modify_item()' method within 'Milestone 2'.
 dict_StoreItems: Dict[str, StoreItem] = {
     'nike romaleos': StoreItem('Nike Romaleos', 'Volt color, Weightlifting shoes', 189.0),
     'chocolate chips': StoreItem('Chocolate Chips', 'Semi-sweet', 3.0),
@@ -57,7 +59,8 @@ def AddNewStoreItem():
     while not validInput or newItemPrice <= 0.0:
         try:
             newItemPrice = float(input('Enter the item price: '))
-            validInput = True
+            if newItemPrice > 0.0:
+                validInput = True
         except ValueError:
             print('(Invalid Input) Please try again')
 
@@ -68,9 +71,9 @@ def AddNewStoreItem():
 # Step 1: Build the ItemToPurchase class with the following specifications
 class ItemToPurchase:
     def __init__(self):
-        self.item_name: str = "none"                # default item_name = "none"
-        self.item_price: float = 0.0                # default item_price = 0.0
-        self.item_quantity: int = 0                 # default  item_quantity = 0
+        self.item_name: str = "none"    # default item_name = "none"
+        self.item_price: float = 0.0    # default item_price = 0.0
+        self.item_quantity: int = 0     # default  item_quantity = 0
         self.Description = ""       # this attribute mentioned in modify_item() method's requirement.
     def print_item_cost(self):
         print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = {self.item_price*self.item_quantity}")
@@ -80,14 +83,17 @@ class ItemToPurchase:
 dict_purchasedItems: Dict[str,ItemToPurchase] = { }
 
 # Step 2: In the main section of your code, prompt the user for two items and create two objects of the ItemToPurchase class.
-def purchaseOneItem(howmany: int = 1):
+def purchaseOneItem(isMilestone1: bool = False, howmany: int = 1):
     newItemName = input("Enter the item name: ").lstrip().rstrip()
     while len(newItemName) < 1 or (newItemName.lower() in dict_purchasedItems):
+        if newItemName.lower() in dict_purchasedItems:
+            print("This item is already present in your cart!!")
         newItemName = input("(try again) Enter the item name: ").lstrip().rstrip()
 
-    newItemDesc = input("Enter the item description: ").lstrip().rstrip()
-    while len(newItemDesc) < 1:
-        newItemDesc = input("(try again) Enter the item description: ").lstrip().rstrip()
+    if isMilestone1 == False:
+        newItemDesc = input("Enter the item description: ").lstrip().rstrip()
+        while len(newItemDesc) < 1:
+            newItemDesc = input("(try again) Enter the item description: ").lstrip().rstrip()
     
     itemPrice: float = 0.0
     validInput = False
@@ -110,7 +116,8 @@ def purchaseOneItem(howmany: int = 1):
 
     itemPurchased = ItemToPurchase()
     itemPurchased.item_name = newItemName
-    itemPurchased.Description = newItemDesc
+    if isMilestone1 == False:
+        itemPurchased.Description = newItemDesc
     itemPurchased.item_quantity = howmany
     itemPurchased.item_price = itemPrice
     dict_purchasedItems[newItemName.lower()] = itemPurchased
@@ -122,19 +129,19 @@ def showTotalCosts(purchasedItems: List[ItemToPurchase]):
     totalCost: float = 0.0
     for oneItem in purchasedItems:
         totalCost = totalCost + (oneItem.item_price)*(oneItem.item_quantity)
-        print(f"{oneItem.item_name} {oneItem.item_quantity} @{oneItem.item_price} = ${oneItem.item_price*oneItem.item_quantity}")
+        print(f"{oneItem.item_name} {oneItem.item_quantity} @ ${oneItem.item_price} = ${oneItem.item_price*oneItem.item_quantity}")
     print(f"Total: ${totalCost}")
     return
 
 
 # Milestone 1
-def Milestone1():
-    clearScreen()
+def Milestone1():   
     dict_purchasedItems.clear()
     print("\t Item 1")
-    purchaseOneItem()
+    purchaseOneItem(isMilestone1=True)
     print("\t Item 2")
-    purchaseOneItem()
+    purchasedItem = purchaseOneItem(isMilestone1=True)
+    # purchasedItem.print_item_cost() # Step 1
     showTotalCosts(dict_purchasedItems.values())
     return
 
@@ -144,7 +151,7 @@ class ShoppingCart:
     def __init__(self):
         self.customer_name = "none"
         self.current_date = "January 1, 2020"
-        self.cart_items: List[ItemToPurchase] = [] # as stated in the problem, it should list. So, here I'll use above 'CartItem' object as each item in the list.
+        self.cart_items: List[ItemToPurchase] = [] # as stated in the problem, it should be list. So, here I'll use above 'ItemToPurchase' object as each item in the list.
     def __init__(self, name:str, date:str):
         self.customer_name = name.lstrip().rstrip()
         self.current_date = date.lstrip().rstrip()
@@ -165,7 +172,7 @@ class ShoppingCart:
             existingCartItem = self.find_item_by_name(itemToPurchaseName)
             if existingCartItem == None:              
                 self.cart_items.append(itemToPurchase) # new item added in the ShoppingCart
-            else:
+            else:  # existing item count updated in the ShoppingCart
                 indx: int = self.cart_items.index(existingCartItem)
                 self.cart_items[indx].item_quantity = self.cart_items[indx].item_quantity + ItemToPurchase.item_quantity                
         return
@@ -184,23 +191,25 @@ class ShoppingCart:
 
     # Modifies an item's description, price, and/or quantity. Has parameter ItemToPurchase.
     # If item Not found (by name) in cart, output this message: Item not found in cart.
-    # If item found in cart, check if parameter has default values for description, price, and quantity. 
-    #     If not, modify item in cart. Otherwise, nothing modified.
-    # To make it more user friendly, we'll assume ItemToPurchase is of type: StoreItem
+    # If item found in cart, check if parameter has default values (as initialized in 'dict_StoreItems' earlier) for description, price, and quantity.
+    #     If not, modify item in cart. Otherwise, nothing modified.    
     def modify_item(self, modifyingItem: ItemToPurchase ):
         if modifyingItem != None and len(self.cart_items) > 0:
             indx = next((i for i, x in enumerate(self.cart_items) if (lambda x: modifyingItem.item_name.lower() == x.item_name.lower()) ))
-            if(indx >= 0):
-                self.cart_items[indx].Description = modifyingItem.Description
-                self.cart_items[indx].item_price = modifyingItem.item_price
-                self.cart_items[indx].item_quantity = modifyingItem.item_quantity            
+            if(indx >= 0): # 'modifyingItem' found in the 'cart_items list' at index 'indx'
+                self.cart_items[indx].Description = dict_StoreItems[modifyingItem.item_name.lower()].Description
+                self.cart_items[indx].item_price = dict_StoreItems[modifyingItem.item_name.lower()].Price
+                self.cart_items[indx].item_quantity = 1 # default value not mentioned in the problem statement, logically assumed to be 1
             else:
                 print("Item not found in cart")
         return
 
     # Returns quantity of all items in cart. Has no parameters.
     def get_num_items_in_cart(self):
-        return len(self.cart_items)
+        totalItemCount: int = 0
+        for anItem in self.cart_items:
+            totalItemCount += anItem.item_quantity 
+        return totalItemCount # len(self.cart_items)
 
     def print_item_cost(self):
         print(self.item_name, str(self.item_quantity), "@ ${:.2f} = ${:.2f}". format(self.item_price, (self.item_price * self.item_quantity)))
@@ -218,11 +227,11 @@ class ShoppingCart:
         if len(self.cart_items) < 1:
             print('SHOPPING CART IS EMPTY')            
             return
-        print(self.customer_name + "'s Shopping Cart - ", self.current_date)
+        print(self.customer_name + "'s Shopping Cart -", self.current_date)
         totalItems = 0
         for anItem in self.cart_items:
-            totalItems += anItem.item_quantity
-        print("Number of Items: ", str(totalItems))
+            totalItems = totalItems + anItem.item_quantity
+        print("Number of Items:", str(totalItems))
         totalCost = 0.0
         for anItem in self.cart_items:
             totalCost += (anItem.item_price * anItem.item_quantity)
@@ -235,12 +244,13 @@ class ShoppingCart:
         if len(self.cart_items) < 1:
             print('SHOPPING CART IS EMPTY')            
             return
-        print(self.customer_name + "'s Shopping Cart - ", self.current_date)
-        print("Item Descriptions")        
+        print(self.customer_name + "'s Shopping Cart -", self.current_date)
+        print("\tItem Descriptions")        
         for anItem in self.cart_items:            
-            print(anItem.item_name, ": ", anItem.Description)        
+            print(anItem.item_name, ":", anItem.Description)
         return
     
+# Step 5:
 # Displays the menu for user action.
 def print_menu():    
     print("a - Add item to cart")
@@ -251,21 +261,23 @@ def print_menu():
     print("q - Quit")    
     return
 
-# Read menu selection by the user.
+# Read menu selection by the user. Step 5
+# User has to enter option only within: 'a', 'r', 'c', 'i', 'o', 'q'
 def readUserSelection():
     userInput = ""
-    while userInput != "arcioq" and len(userInput) != 1:
+    while len(userInput) != 1 or userInput not in "arcioq":
         userInput = input('Choose an option: ').lstrip().rstrip().lower()
     return userInput
 
 # the main function that calls other functions and does the final work
-def module8pp_OnlineShoppingCart():
-    clearScreen()
+def Milestone2():
+    dict_purchasedItems.clear()
     print("\n === Shopping Cart Demo ===")
     ShowStoreItems()
-    #AddNewStoreItem()  # works ok
-    #ShowStoreItems()
+    #AddNewStoreItem() # works ok
+    #ShowStoreItems()  # works ok
     
+    # Step 7 begins
     print("\n")
     customerName = ""
     userEnteredDate = ""
@@ -284,39 +296,45 @@ def module8pp_OnlineShoppingCart():
     
     print(f"Customer name: {ShoppingCart_obj.customer_name}")
     print(f"Today's date: {ShoppingCart_obj.current_date}\n")
+    # Step 7 ends
 
-    print_menu()
+    print_menu() # Step 5.
     userSelection = readUserSelection()
     while userSelection != 'q':
-        if userSelection == 'a':  # a - Add item to cart
-            print("Add item to cart:")
+        if userSelection == 'a':  # a - Add item to cart. Step 8.
+            print("ADD ITEM TO CART")
             newcartItem = purchaseOneItem()
             ShoppingCart_obj.add_item(newcartItem)
-        elif userSelection == 'r': # r - Remove item from cart
+        elif userSelection == 'r': # r - Remove item from cart. Step 9.
+            print("REMOVE ITEM FROM CART")
             removeItemName = ""
             while len(removeItemName) < 1:
-                removeItemName = input("Please enter the item name, you want to remove from cart: ").lstrip().rstrip()
+                removeItemName = input("Enter name of item to remove:").lstrip().rstrip()
             ShoppingCart_obj.remove_item(removeItemName)
-        elif userSelection == 'c': # c - Change item quantity
+        elif userSelection == 'c': # c - Change item quantity. Step 10.
+            print("CHANGE ITEM QUANTITY")
             changeItemName = ""
             while len(changeItemName) < 1:
-                changeItemName = input("Please enter item name for which you want to change the quantity: ").lstrip().rstrip()
+                changeItemName = input("Enter the item name:").lstrip().rstrip()
             foundCartItem = ShoppingCart_obj.find_item_by_name(changeItemName)
             if foundCartItem == None:
                 print("This item not found in your current cart:")
             else:
                 howmany: int = 0
                 validInput = False
-                while howmany < 0 and not validInput:
+                while howmany < 0 or validInput == False:
                     try:
-                        howmany = int(input('How many of this item you want to keep? '))
-                        validInput = True
+                        howmany = int(input("Enter the new quantity:"))
+                        if howmany > 0:
+                            validInput = True
                     except ValueError:
                         print('(Invalid Input) Please try again')
                 ShoppingCart_obj.cart_items[ShoppingCart_obj.cart_items.index(foundCartItem,0)].item_quantity = howmany
         elif userSelection == 'i': # i - Output items' descriptions
+            print("\tOUTPUT ITEMS' DESCRIPTIONS")
             ShoppingCart_obj.print_descriptions()
-        elif userSelection == 'o':  # o - Output shopping cart
+        elif userSelection == 'o':  # o - Output shopping cart. Step 6
+            print("\tOUTPUT SHOPPING CART")
             ShoppingCart_obj.print_total()
 
         print("\nPlease Choose your next menu option: ")
@@ -327,5 +345,8 @@ def module8pp_OnlineShoppingCart():
 
 
 if __name__ == "__main__":
-    #Milestone1()
-    module8pp_OnlineShoppingCart()
+    clearScreen()
+    print("===== Milestone1 =====")
+    Milestone1()
+    print("\n===== Milestone2 =====")
+    Milestone2()
