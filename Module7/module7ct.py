ac4e9fd93fc0bb3d0938527c5f6222b05771abc9
csu-mm <mukul.mondal@csuglobal.edu>
2026-01-07 12:50:08 -0500

    Program heading added
    Program heading added

--- Module7/module7ct.py
+++ Module7/module7ct.py
@@ -1,3 +1,14 @@
+'''
+Colorado State University Global
+( https://csuglobal.edu/academic-programs/graduate-degrees/masters-science-degree-artificial-intelligence-machine-learning )
+MS - Artificial Intelligence and Machine Learning
+Course: CSC500 - Principles of Programming
+Module 7: Critical Thinking Assignment
+Professor: Dr. Steven A. Evans
+Created by Mukul Mondal
+February 25, 2025
+'''
+
 from os import system, name
 from typing import Dict
 
